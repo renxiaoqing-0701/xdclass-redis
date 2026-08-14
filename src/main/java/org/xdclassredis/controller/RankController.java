f4451b86ab34d64019cec08764a12526a9438926
renxiaoqing <renxiaoqing@linklogis.com>
2026-08-14 17:55:47 +0800

    List热销实战

---
+++ src/main/java/org/xdclassredis/controller/RankController.java
@@ -0,0 +1,30 @@
+package org.xdclassredis.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.xdclassredis.model.VideoDo;
+import org.xdclassredis.util.JsonData;
+
+import java.util.List;
+
+/**
+ * @Description List实战排行榜
+ * @Author renxiaoqing
+ * @Date 2026/8/14 17:36
+ * @Version 1.0
+ **/
+@RestController
+@RequestMapping("/api/v1/rank")
+public class RankController {
+    @Autowired
+    private RedisTemplate redisTemplate;
+    @GetMapping("daily_rank")
+    public JsonData videoRank(){
+        String DAILY_RANK_KEY = "video:rank:daily";
+        List<VideoDo> list = redisTemplate.opsForList().range(DAILY_RANK_KEY,0,-1);
+        return JsonData.buildSuccess(list);
+    }
+}
--- src/test/java/org/xdclassredis/test/XdclassRedisApplicationTests.java
+++ src/test/java/org/xdclassredis/test/XdclassRedisApplicationTests.java
@@ -6,7 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.xdclassredis.model.UserDo;
+import org.xdclassredis.model.VideoDo;
 
+import java.math.BigDecimal;
 import java.util.concurrent.TimeUnit;
 
 @SpringBootTest
@@ -32,5 +34,26 @@ public class XdclassRedisApplicationTests {
     public void testDLock(){
        boolean flag =  stringRedisTemplate.opsForValue().setIfAbsent("coupon:1001","100",30, TimeUnit.SECONDS);
        System.out.println("获取锁:"+flag);
+    }
+    @Test
+    public void saveRank(){
+        String DAILY_RANK_KEY = "video:rank:daily";
+        VideoDo video1 = new VideoDo(3L,"PaaS工业级微服务大课","xdclass.net",new BigDecimal(1099));
+        VideoDo video2 = new VideoDo(5L,"AlibabaCloud全家桶实战","xdclass.net",new BigDecimal(59));
+        VideoDo video3 = new VideoDo(53L,"SpringBoot2.X+Vue3综合实战","xdclass.net",new BigDecimal(49));
+        VideoDo video4 = new VideoDo(15L,"玩转23种设计模式+最近实战","xdclass.net",new BigDecimal(99));
+        VideoDo video5 = new VideoDo(45L,"Nginx网关+LVS+KeepAlive","xdclass.net",new BigDecimal(89));
+        redisTemplate.opsForList().leftPushAll(DAILY_RANK_KEY,video4,video5,video3,video2,video1);
+    }
+
+    /**
+     * 替换榜单第二名
+     */
+    @Test
+    void replaceRank(){
+        String DAILY_RANK_KEY = "video:rank:daily";
+        VideoDo video = new VideoDo(42L,"小滴课堂面试专题第一季高级工程师","xdclass.net",new BigDecimal(89));
+        //在集合的指定位置插入元素,如果指定位置已有元素，则覆盖，没有则新增
+        redisTemplate.opsForList().set(DAILY_RANK_KEY,1,video);
     }
 }
